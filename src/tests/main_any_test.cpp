91532c76da2e8dad2685f370973ba64de305b901
Davit Kalantaryan <davit.kalantaryan@gmail.com>
2021-12-09 09:20:44 +0100

    modified to run also secure server

--- src/tests/main_any_test.cpp
+++ src/tests/main_any_test.cpp
@@ -8,16 +8,40 @@ int main(int a_argc, char* a_argv[])
 	QCoreApplication aApp(a_argc,a_argv);
 	QHttpServer httpServer;
 	
+	qDebug()<<QSslConfiguration::defaultConfiguration().isNull();
+	
+	QSslConfiguration sslConfiguration;
+	//QFile certFile(QStringLiteral("./localhost.cert"));
+	//QFile keyFile(QStringLiteral("./localhost.key"));
+	QFile certFile(QStringLiteral("D:\\davit\\dev\\qthttpserver_handler\\.tests\\certificate.crt"));
+	QFile keyFile(QStringLiteral("D:\\davit\\dev\\qthttpserver_handler\\.tests\\privatekey.key"));
+	certFile.open(QIODevice::ReadOnly);
+	keyFile.open(QIODevice::ReadOnly);
+	QSslCertificate certificate(&certFile, QSsl::Pem);
+	QSslKey sslKey(&keyFile, QSsl::Rsa, QSsl::Pem);
+	certFile.close();
+	keyFile.close();
+	sslConfiguration.setPeerVerifyMode(QSslSocket::VerifyNone);
+	sslConfiguration.setLocalCertificate(certificate);
+	sslConfiguration.setPrivateKey(sslKey);
+	sslConfiguration.setProtocol(QSsl::TlsV1SslV3);
+	httpServer.sslSetup(sslConfiguration);
+	
 	httpServer.route("/", []() {
         return "Hello world";
     });
 	
-	httpServer.route("/files/<arg>", [] (const QUrl &url) {
-		qDebug()<<"New querry: "<<url;
-        return QHttpServerResponse::fromFile(url.toString());
-    });
+	//httpServer.route("/files/<arg>", [] (const QUrl &url) {
+	//	qDebug()<<"New querry: "<<url;
+    //    return QHttpServerResponse::fromFile(url.toString());
+    //});
 	
-	const auto port = httpServer.listen(QHostAddress::Any);
+	QObject::connect(&httpServer,&QHttpServer::missingHandler,&httpServer,[](const QHttpServerRequest &request, QTcpSocket *socket){
+		qDebug()<<"!!! request:"<<request;
+		qDebug()<<"+++ socket:"<< socket;
+	});
+	
+	const auto port = httpServer.listen(QHostAddress::Any,443);
     if (!port) {
         qDebug() << QCoreApplication::translate(
                 "QHttpServerExample", "Server failed to listen on a port.");
